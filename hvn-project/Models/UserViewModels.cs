ac462fbef4993647ac19a4f385b278614eb3162c
Alex Jussiani Junior <53544026+AlexJussiani@users.noreply.github.com>
2022-07-24 16:00:51 -0300

    Ajuste na classe UsuarioRegistro

--- hvn-project/Models/UserViewModels.cs
+++ hvn-project/Models/UserViewModels.cs
@@ -12,9 +12,6 @@ namespace hvn_project.Models
         public string Nome { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
-        public string Cpf { get; set; }
-
-        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
         public string Email { get; set; }
 
