12fc66efd2f1a435e98a6dedcf4450e0d349b4f0
Leonardo Silva Walder <42324501+LeonardoSW@users.noreply.github.com>
2023-08-09 19:05:10 -0300

    Update SettingsVariables.cs

--- hvn-project/SettingsVariables.cs
+++ hvn-project/SettingsVariables.cs
@@ -2,7 +2,7 @@
 {
     public class SettingsVariables
     {
-        public static string Domain = "mongodb+srv://hvn-mongodb-test:havan@cluster0.0z4iu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+        public static string Domain = "mongodb+srv://hvn-mongodb-test:havan@cluster0.0z4iu.mongodb.net/{dbname}?retryWrites=true&w=majority";
         public static string DatabaseName = "havan";
         public static string Collection = "PatrimonyItems";
     }
